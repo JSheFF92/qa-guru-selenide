8f77f1c66553478e066868b8e34e260b4983be97
Evgenyi <shevchuk291992@gmail.com>
2023-07-25 20:45:02 +0300

    fis test

--- src/test/java/SelenidRepositoryWiki.java
+++ src/test/java/SelenidRepositoryWiki.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -29,9 +30,15 @@ public class SelenidRepositoryWiki {
         open("https://github.com/");
         $("[data-target='qbsearch-input.inputButtonText']").click();
         $("[data-target='query-builder.input']").setValue("selenide").pressEnter();
-        $(".search-results-page").$(byText("More")).click();
-        $("[data-testid='nav-item-wikis']").click();
-        $("a[href='/YuriKopshev/selenide/wiki/SoftAssertions'].bJBoUI").click();
+
+        $("div.search-title a").click();
+        $("#wiki-tab").click();
+
+        $(".f6.Link--muted.js-wiki-more-pages-link.btn-link.mx-auto").click();
+        $("a[href='/selenide/selenide/wiki/SoftAssertions']").shouldHave(text("SoftAssertions"));
+
+        $("[href='/selenide/selenide/wiki/SoftAssertions']").click();
+
         $(".markdown-body").
                 $(byText("3. Using JUnit5 extend test class:"));
         $(".markdown-body")
